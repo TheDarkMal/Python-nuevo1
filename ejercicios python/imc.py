2f4ccfeba91788a33c969b031f6e2d63fd0caa93
Mauricio Vergara <mauricio301051020@gmail.com>
2019-07-24 22:06:37 -0500

    coriigiendo ultimo ejercicio

--- ejercicios python/imc.py
+++ ejercicios python/imc.py
@@ -1,21 +1,21 @@
 print("Bienvenido a imc calculator")
 
-
-altura=int(print("digite su altura"))
+peso=int(input("digite su peso: "))
+altura=float(input("digite su altura: "))
 
 imc=peso/(altura*2)
-
+print("tu imc es: ",imc)
 if imc <18.5:
     print("esta bajo de peso")
-elif imc ==18.5 and imc<=24.9:
+elif imc >=18.5 and imc<=24.9:
     print("esta normal de peso")
-elif imc==25 and imc <=29.9 :
+elif imc>=25 and imc <=29.9 :
     print("esta sobrepeso de peso")
-elif imc ==300 and imc <=34.9:
+elif imc >=30 and imc <=34.9:
     print("esta en ovesidad 1")
-elif imc ==35 and imc<=39.9:
+elif imc >=35 and imc<=39.9:
     print("esta ovesidad 2")
-elif imc ==40 and imc <=49.9:
+elif imc >=40 and imc <=49.9:
     print("esta en ovesidad 3")
 elif imc >50:
     print("esta en ovesidad 4")
